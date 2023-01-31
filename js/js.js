72c63ef61e19ebfcbd9407699b799548fff346d6
Bruno <brunosabrito2@gmail.com>
2023-01-31 14:53:51 -0300

    Make function where the computer choose between rock, paper or scissor

---
+++ js/js.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    const escolha = Math.floor(Math.random() * 3);
+    
+    if( escolha == 2){
+        return "rock"
+    }else if (escolha == 1){
+        return "paper"
+    } else if (escolha == 0){
+        return "scissors"
+    }
+    
+}
+console.log(getComputerChoice())
+//const playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase()
+
+  
+
+
