7384b63c859407e41f4ffc8128cf487116ee91d6
Bruno <brunosabrito2@gmail.com>
2023-01-31 15:10:31 -0300

    added prompt player choice

--- js/js.js
+++ js/js.js
@@ -1,18 +1,13 @@
 function getComputerChoice() {
     const escolha = Math.floor(Math.random() * 3);
-    
     if( escolha == 2){
         return "rock"
     }else if (escolha == 1){
         return "paper"
     } else if (escolha == 0){
         return "scissors"
-    }
-    
+    } 
 }
-console.log(getComputerChoice())
-//const playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase()
-
-  
-
+const computerChoice = getComputerChoice()
+const playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase()
 
