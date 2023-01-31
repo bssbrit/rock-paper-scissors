571ede9b330cc4e9feb5ac4700f20f3b4843f2df
Bruno <brunosabrito2@gmail.com>
2023-01-31 15:25:47 -0300

    Finish the round function

--- js/js.js
+++ js/js.js
@@ -10,4 +10,18 @@ function getComputerChoice() {
 }
 const computerChoice = getComputerChoice()
 const playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase()
-
+console.log(computerChoice) 
+function round() {
+    if (playerChoice == computerChoice){
+        return("TIE")
+    } else if (playerChoice == "rock" && computerChoice == "scissors"){
+        return("You win!!")
+    }  else if (playerChoice == "paper" && computerChoice == "rock"){
+        return("You win!!")
+    } else if (playerChoice == "scissors" && computerChoice == "paper"){
+        return("You win!!")
+    } else {
+        return("You lose")
+    }
+}
+ round ()
