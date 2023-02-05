f9e85dbab2a89846cb451321591ca1d2fa963bd0
Bruno <brunosabrito2@gmail.com>
2023-02-05 17:05:45 -0300

    remade the js file from scratch

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
-function getComputerChoice() {
+function computerChoice() {
     const escolha = Math.floor(Math.random() * 3);
-    if( escolha == 2){
+    if( escolha === 2){
         return "rock"
     }else if (escolha == 1){
         return "paper"
@@ -8,41 +8,74 @@ function getComputerChoice() {
         return "scissors"
     } 
 }
+console.log(computerChoice())
+
+function playerChoice() {
+    prompt("Rock, Paper or Scissors").toLowerCase()
+    if (prompt() === "rock" || prompt() === "paper" || prompt() === "scissors") {
+        alert("DON'T DO THIS MAN")
+        playerChoice()
+    }
+}
+
+playerChoice()
+
+
+
+
+/*
 let computerChoice = getComputerChoice()
 let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase()
 console.log(computerChoice) 
+/*
 function round() {
     if (playerChoice == computerChoice){
         return("TIE")
-    } else if (playerChoice == "rock" && computerChoice == "scissors"){
-        return ("You win!")
-
-    }  else if (playerChoice == "paper" && computerChoice == "rock"){
-        return("You win!")
-    } else if (playerChoice == "scissors" && computerChoice == "paper"){
-        return("You win!")
+        console.log("TIE")
+    } else if (playerChoice === "rock" && computerChoice === "scissors"){
+        return ("You've won this round!")
+        console.log("You've won this round!")
+       
+    }  else if (playerChoice === "paper" && computerChoice === "rock"){
+        return("You've won this round!")
+        console.log("You've won this round!")
+        
+    } else if (playerChoice === "scissors" && computerChoice === "paper"){
+        return("You've won this round!")
+        console.log("You've won this round!")
+        
     } else {
-        return("You lose")
+        return("You've lost this round")
+        console.log("You've lost")
     }
 }
+/*
+round()
 
 let result = round()
- 
+let wins = 0
+let losts = 0
 
-function play() {
-    for (let i = 0; i < 5; i++) {
-        if (result == "You win!"){
-    
-         console.log("you've won this round")
-        } else if (result == "You lose"){
-            i--
-            console.log("you've lost this round")
-        } else { 
-            console.log("tie")
+function play(numberOfMatches){
+    for (let matches = 0; matches <= numberOfMatches; matches = matches + 1){
+        round();
+        let result = round();
+        if (result === "You've won this round!") { 
+            return wins = losts + 1;
+            console.log(wins);
+        } else if (result === "You've lost this round") {           
+            return losts = losts + 1;
+            console.log(losts);
+        } else {
+            console.log("Tie")
         }
-
+    } if (wins > losts){
+        console.log("congratulations")
+    } else if (losts > wins){
+        console.log("Sorry")
     }
-    
 }
+*/
 
-play()
+
+                                 
